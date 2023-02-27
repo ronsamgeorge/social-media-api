e42854b43465d2b07617454a7468b6a561c50b7e
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-27 23:31:42 +0530

    Get comments for posts

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,8 @@
 import express from "express";
-import {} from "../controllers/comment.js";
+import { getComments } from "../controllers/comment.js";
 
 const router = express.Router();
 
-router.get("");
+router.get("/", getComments);
 
 export default router;
