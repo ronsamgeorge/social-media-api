e34ed65013d3c0070dd96e6c21da4409e66055c8
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-28 00:05:34 +0530

    Post comment for Post

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,4 +1,6 @@
 import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+import moment from "moment";
 
 export const getComments = (req, res) => {
   const q =
@@ -7,5 +9,31 @@ export const getComments = (req, res) => {
   db.query(q, [req.query.postId], (err, data) => {
     if (err) return res.status(500).json(err);
     return res.status(200).json(data);
+  });
+};
+
+export const addComments = (req, res) => {
+  // retrive user id from access token
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not Logged in");
+
+  // verify token
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, userInfo) => {
+    if (err) return res.status(403).json("Invalid token");
+
+    const q =
+      "INSERT INTO comments (`des`,`createdAt`, `userId`, `postId`) VALUES (?)";
+
+    const values = [
+      req.body.desc,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+      req.body.postId,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Comment has been created");
+    });
   });
 };
