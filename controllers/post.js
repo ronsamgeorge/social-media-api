30db3e755aee3b7f1e4b1b25a58a4dd71e1122bc
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-27 14:45:28 +0530

    Add Image post functionality

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,6 @@
 import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
+import moment from "moment";
 
 export const getPosts = (req, res) => {
   // retrive user id from access token
@@ -16,6 +17,32 @@ export const getPosts = (req, res) => {
     db.query(q, [userInfo.id, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data);
+    });
+  });
+};
+
+export const addPost = (req, res) => {
+  // retrive user id from access token
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not Logged in");
+
+  // verify token
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, userInfo) => {
+    if (err) return res.status(403).json("Invalid token");
+
+    const q =
+      "INSERT INTO posts (`desc`, `img`,`createdAt`, `userId`) VALUES (?)";
+
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("post has been created");
     });
   });
 };
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import commentRoute from "./routes/comments.js";
 import authRoute from "./routes/auth.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
@@ -27,6 +28,22 @@ app.use(
   })
 );
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
