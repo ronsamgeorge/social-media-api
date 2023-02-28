dd527c580ccad08e789bd4e3616830ff040c2bd1
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-28 15:54:54 +0530

    Create and delete likes on post

--- controllers/like.js
+++ controllers/like.js
@@ -10,3 +10,40 @@ export const getLikes = (req, res) => {
     return res.status(200).json(data.map((like) => like.userId));
   });
 };
+
+export const addLike = (req, res) => {
+  // retrive user id from access token
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not Logged in");
+
+  // verify token
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, userInfo) => {
+    if (err) return res.status(403).json("Invalid token");
+
+    const q = "INSERT INTO likes (`userId`, `postId`) VALUES (?)";
+    const values = [userInfo.id, req.body.postId];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been liked");
+    });
+  });
+};
+
+export const deleteLike = (req, res) => {
+  // retrive user id from access token
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not Logged in");
+
+  // verify token
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, userInfo) => {
+    if (err) return res.status(403).json("Invalid token");
+
+    const q = "DELETE FROM likes WHERE `userId` = ? AND `postId` = ? ";
+
+    db.query(q, [userInfo.id, req.query.postId], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been un-liked");
+    });
+  });
+};
